5a0f6a89e4768374d9de8a80c884a9521f598068
Abdul Samad <abdulsamadmughal102@gmail.com>
2025-02-17 19:37:32 +0500

    counter app

---
+++ ReactJs/counter/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Counter from './Component/Counter'
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>this is aap</h1>
+      <Counter />
+    </div>
+  )
+}
