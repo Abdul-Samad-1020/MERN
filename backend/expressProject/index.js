2a48ce58df410dcaa3f6dc929827dd1b2e901b46
Abdul Samad <abdulsamadmughal102@gmail.com>
2025-03-14 19:02:47 +0500

    update

--- backend/expressProject/index.js
+++ backend/expressProject/index.js
@@ -5,11 +5,15 @@ let app = express();
 app.use(express.json());
 
 // Handle POST request for login
-app.post('/login', (req, res) => {
+app.get('/login', (req, res) => {
     console.log(req.body);  // Log the request body
     res.send({status: 1, message: "Login success", data: req.body});
 });  
 
+app.get('/news/:id', (req, res) => {
+let news = req.params.id;
+res.send({status: 1, message: "hello", data: news});
+});
 // Start server on port 800
 app.listen(800, () => {
     console.log("Server is running on http://localhost:800");
