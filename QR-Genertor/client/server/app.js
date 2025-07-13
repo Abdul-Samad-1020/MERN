b1348d326ac73c753fba2cb2b86064e1d63ceda5
Abdul Samad <abdulsamadmughal102@gmail.com>
2025-07-14 01:16:19 +0500

    dfas

---
+++ QR-Genertor/client/server/app.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+
+
+const app = express()
+app.use(bodyparser.json())
+app.use(cors({
+    origin:'https://localhost:3000'
+}))
+
+
+
+const port = process.env.PORT ;
+app.listen(port,(req,res)=>{
+    console.log(`server is runing on ; ${port}`)
+})
