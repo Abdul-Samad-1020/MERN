e9a8f2e41e3f404ffec67f5fd1215821fca5b26d
Abdul Samad <abdulsamadmughal102@gmail.com>
2025-07-14 02:00:31 +0500

    Update user.js

--- QR-Genertor/client/server/Model/user.js
+++ QR-Genertor/client/server/Model/user.js
@@ -1,28 +1,23 @@
-
-const mongoose = require ('mongoose')
-
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    user_name:{
-        type:String,
-        required : true,
+  user_name: {
+    type: String,
+    required: true,
+  },
+  user_email: {
+    type: String,
+    required: true,
+  },
+  user_pass: {
+    type: String,
+    required: true,
+  },
+  user_status: {
+    type: String,
+    enum: ['enable', 'disable'],
+    default: 'enable',
+  }
+});
 
-    },
-    user_email:{
-        type:string,
-        required :true,
-
-    },
-    user_pass:{
-        type : string,
-        required: true,
-    },
-    user_status:{
-        type:String,
-        enum:['enable', 'disabe'],
-        default: 'enable'
-    }  
-})
-
-
-module.exports = mongoose.model('user_mst', userSchema)
+module.exports = mongoose.model('user_mst', userSchema);
